5d3102de6d197caf699001bc56f9f26c7ae70ba7
sai <sai@DESKTOP-3S42E7Q>
2018-05-09 13:51:39 +0530

    commitdemo

---
+++ JenkinsGitDemo/src/jenkinsGitDemo/JenkinsGitDemo.java
@@ -0,0 +1,9 @@
+package jenkinsGitDemo;
+
+import org.testng.annotations.Test;
+
+public class JenkinsGitDemo {
+@Test
+public void myJenkinsGit()
+{for(int i =0;i<10;i++) {System.out.println(i+"="+"Welcome Git & Jenkins");}}
+}
